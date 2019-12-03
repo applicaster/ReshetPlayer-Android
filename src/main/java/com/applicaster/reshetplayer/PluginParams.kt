3cbb9542a05c56e8d9da69a1554b4a7c8daaf9a3
shay markovich <shaymark@gmail.com>
2019-12-03 19:20:21 +0200

    add tests, update sdk, change params config

--- src/main/java/com/applicaster/reshetplayer/PluginParams.kt
+++ src/main/java/com/applicaster/reshetplayer/PluginParams.kt
@@ -1,7 +1,5 @@
 package com.applicaster.reshetplayer
 
-import com.applicaster.app.CustomApplication
-
 object PluginParams {
 
     var artimediaSiteName: String = ""
@@ -18,13 +16,13 @@ object PluginParams {
     internal val CONF_ARTIMEDIA_SITE_KEY = "artimedia_site_key"
     internal val CONF__DANTAR_SITE_KEY = "kantar_site_key"
     internal val CONF_SHOW_ADS_ON_PAYED = "show_ads_on_payed"
-    internal val CONF_LIVE_STREAM_URL = "c1_cut_time"
+    internal val CONF_LIVE_STREAM_URL = "live_stream_url"
     internal val CONF_C1_CUT_TIME = "c1_cut_time"
     internal val CONF_C1_WINDOW_LENGTH_TIME = "c1_window_length_time"
     internal val CONF_SERVER_TIME_URL = "server_time_url"
-    internal val CONF_PLAYER_VERSION = "player_version_name"
-    internal val CONF_PLAYER_NAME = "player_version_name"
-    internal val CONF_KANTAR_ATRRIBUTE_STREAM_VALUE = "kantar attribute stream value"
+    internal val CONF_KANTAR_PLAYER_VERSION = "kantar_player_version"
+    internal val CONF_KANTAR_PLAYER_NAME = "kantar_player_name"
+    internal val CONF_KANTAR_ATRRIBUTE_STREAM_VALUE = "kantar_attribute_stream_value"
 
     fun initParams(pluginConfiguration: MutableMap<Any?, Any?>) {
         kantarSiteName = pluginConfiguration[CONF__DANTAR_SITE_KEY].toString()
@@ -34,8 +32,8 @@ object PluginParams {
         c1_cut_time = parseTime(pluginConfiguration[CONF_C1_CUT_TIME].toString())
         c1_window_length_time = pluginConfiguration[CONF_C1_WINDOW_LENGTH_TIME].toString().toIntOrNull() ?: 36
         serverTimeUrl = pluginConfiguration[CONF_SERVER_TIME_URL].toString()
-        playerVersion = pluginConfiguration[CONF_PLAYER_VERSION].toString()
-        playerName = pluginConfiguration[CONF_PLAYER_NAME].toString()
+        playerVersion = pluginConfiguration[CONF_KANTAR_PLAYER_VERSION].toString()
+        playerName = pluginConfiguration[CONF_KANTAR_PLAYER_NAME].toString()
         kantarAttributeStreamValue = pluginConfiguration[CONF_KANTAR_ATRRIBUTE_STREAM_VALUE].toString()
     }
 

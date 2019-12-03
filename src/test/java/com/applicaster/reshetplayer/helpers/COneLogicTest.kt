b03b8530818c5d2cd0ac4f757a7530ad46dd364f
shaymark <shaymark@gmail.com>
2019-12-03 19:26:38 +0200

    add tests, update sdk, change params config (#8)

--- src/test/java/com/applicaster/reshetplayer/helpers/COneLogicTest.kt
+++ src/test/java/com/applicaster/reshetplayer/helpers/COneLogicTest.kt
@@ -32,6 +32,13 @@ class COneLogicTest {
         //with server delta time minus one hour
         //inside window length outside C+1 timezone israel
         assertEquals(isInOne(parseDate("2019-12-02T02:33:44+0200"), -3600000, parseDate("2019-12-01T01:33:44+0200"), 0, 0, 3600000 * 36), true)
+
+        //inside window length inside C+1 timezone israel summer daytime (+0300)
+        assertEquals(isInOne(parseDate("2019-12-02T02:33:44+0300"), 0, parseDate("2019-12-01T02:33:44+0300"), 23, 0, 3600000 * 36), true)
+
+        //inside window length outside C+1 timezone israel summer daytime (+0300)
+        assertEquals(isInOne(parseDate("2019-12-02T02:33:44+0300"), 0, parseDate("2019-12-01T01:33:44+0300"), 23, 0, 3600000 * 36), false)
+
     }
 
 
